ad52182a773557f4b25d893cd0a48b28d5ff78eb
NicoFox01 <nicolas.g1992@psi.uba.ar>
2026-04-26 21:24:31 -0300

    se crean los modelos de la bbdd, se crea funcionalidad de auth + funcionalidad de users

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.users import User
+from app.models.companies import Company
+from app.models.jobs import Job
+from app.models.candidates import Candidate
+from app.models.applications import Application
+from app.models.logs import KeepAliveLog
